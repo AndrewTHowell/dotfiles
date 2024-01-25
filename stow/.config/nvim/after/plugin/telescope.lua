d51b3ccaee43d4da3dd115518f905c92d2448e61
ahowell <ahowell@thoughtmachine.net>
2024-01-25 08:54:23 +0000

    Better telescope searching

--- stow/.config/nvim/after/plugin/telescope.lua
+++ stow/.config/nvim/after/plugin/telescope.lua
@@ -1,8 +1,23 @@
+require('telescope').setup{
+    pickers = { 
+        find_files = {
+            hidden = true,
+            file_ignore_patterns = {".*.git/.*"}
+        },
+        grep_string= {
+            additional_args = function(opts)
+                return {
+                    "--hidden",
+                    "--glob", "!{**/.git/*}",
+                }
+            end
+        }
+    }
+}
+
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<leader>pf', function() 
-	builtin.find_files({ hidden = true, file_ignore_patterns = {".*.git/.*"} });
-end, { desc = "Project Find: find a file within the project" })
+vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = "Project Find: find a file within the project" })
 vim.keymap.set('n', '<leader>pgf', builtin.git_files, { desc = "Project Git Find: find a file within the git project"})
 vim.keymap.set('n', '<leader>ps', function() 
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
