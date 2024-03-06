a0141a29fc7d5f6dd16dd59b886bc4011504ba99
Andrew Howell <howelldrew99@gmail.com>
2024-03-06 02:19:24 +0000

    Skeleton of what to build first done

--- install.sh
+++ install.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
+# Install Git from source (get version with Signing, git>=2.3)
+
+# Install curl? (The --output-dir option is available since curl 7.73.0)
+
+# Install neovim from source
+
+# Install zsh?
+
+# Install Oh My Zsh
+
 
