ebb503fe80b5cae728a7c917bd5cf5c685f62e8e
Andrew Howell <andrewthomashowell@gmail.com>
2024-03-08 00:18:13 +0000

    Cloning and deploying nvim

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-sudo apt update
+# Apt
+#sudo apt update
+
+pkgs="zsh ripgrep stow curl make cmake gettext unzip"
+sudo apt install $pkgs
 
 # SSH
 ssh_filename="id_ed25519"
@@ -29,8 +33,13 @@ git config --global user.email "andrewthomashowell@gmail.com"
 
 
 # Install curl? (The --output-dir option is available since curl 7.73.0)
-
-# Install neovim from source
+ 
+#NeoVim
+sudo mkdir -p /opt/neovim/src
+sudo git clone --depth 1 --branch stable https://github.com/neovim/neovim.git /opt/neovim/src
+sudo make -C /opt/neovim/src CMAKE_BUILD_TYPE=RelWithDebInfo CMAKE_INSTALL_PREFIX=/opt/neovim
+sudo make -C /opt/neovim/src install
+sudo ln -fsv /opt/neovim/bin/nvim /usr/local/bin/nvim
 
 # Install zsh?
 
