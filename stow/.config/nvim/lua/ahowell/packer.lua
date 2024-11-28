ab94cfa7dd9b1e85441c56759a0a3c101cf3e2b0
Andrew Howell <andrewthomashowell@gmail.com>
2024-11-28 23:09:27 +0000

    Bootstrap packer

--- stow/.config/nvim/lua/ahowell/packer.lua
+++ stow/.config/nvim/lua/ahowell/packer.lua
@@ -1,4 +1,15 @@
--- This file can be loaded by calling `lua require('plugins')` from your init.vim
+local ensure_packer = function()
+    local fn = vim.fn
+    local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+        vim.cmd [[packadd packer.nvim]]
+        return true
+    end
+    return false
+end
+
+local packer_bootstrap = ensure_packer()
 
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
@@ -73,6 +84,12 @@ return require('packer').startup(function(use)
           require('Comment').setup()
       end
   }
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
   
 end)
 
