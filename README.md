5ac2b8afefb145207b58f3df452eef751bb15f21
Andrew Howell <andrewthomashowell@gmail.com>
2024-11-29 00:03:33 +0000

    Cleaner

--- README.md
+++ README.md
@@ -2,22 +2,10 @@
 
 This project aims to capture all configuration files I use and centralise them, with common tooling for easily transferring and deploying config to new machines.
 
-## Stow 
-
-The top level `stow` folder contains config that can be 'stowed' directly into
-your machine using `stow`. The intended directory structure is symlinked exactly.
-
-### Using `stow` 
-
-To deploy this to a new machine, run 
+## Install
 
 ```
-stow --target ~ -vv --stow stow
-```
-
-To redeploy changes to this machine, run 
-
-```
-stow --target ~ -vv --restow stow
+git clone https://github.com/AndrewTHowell/dotfiles ~/dotfiles
+sudo ~/dotfiles/scripts/install.sh
 ```
 
---
+++ scripts/install.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+# clone_or_pull <repo> <dir>
+# Checks if repo exists already. If not, clone it. If it does, pull it.
+clone_or_pull() {
+    if [ ! -d "$2" ]; then
+        git clone --depth 1 "$1" "$2"
+    else
+        git -C "$2" pull
+    fi
+}
+
+echo "Updating package manager"
+sudo apt update
+
+packages="zsh git tmux ripgrep stow curl make cmake gcc"
+echo "Installing required packages: $packages"
+sudo apt install -y $pkgs
+
+echo "Installing fzf (apt version is outdated)"
+clone_or_pull https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+# Add the following to zsh:
+## Set up fzf key bindings and fuzzy completion
+#source <(fzf --zsh)
+
+echo "Installing zsh"
+if [ ! -d ~/.oh-my-zsh ]; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+else
+    ZSH=~/.oh-my-zsh ~/.oh-my-zsh/tools/upgrade.sh
+fi
+
+echo "Installing tmux plugin manager"
+clone_or_pull https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+echo "Installing nvim"
+# Make a quick scratch dir
+mkdir -p ~/scratch
+# Clone/pull dir into scratch dir
+clone_or_pull https://github.com/neovim/neovim ~/scratch/neovim
+# Configure make to include debug info (neovim crash still provides backtrace) and to install in optional add-on area of linux for neovim
+make CMAKE_BUILD_TYPE=RelWithDebInfo CMAKE_INSTALL_PREFIX=/opt/neovim 
+# Install from the scratch dir
+sudo make --directory ~/scratch/neovim install
+# Symlink the binary over to local
+sudo ln -sfv /opt/neovim/bin/nvim /usr/local/bin/nvim
+
+echo "Restowing files"
+~/dotfiles/scripts/restow.sh
+
+echo "Installation complete"
+exec zsh
