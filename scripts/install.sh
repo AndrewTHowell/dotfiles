e647085fd57ca583b15cbb72cacccf0f3eda8513
Andrew Howell <andrewthomashowell@gmail.com>
2025-06-07 18:47:13 +0100

    Move zshrc to allow stow to succeed

--- scripts/install.sh
+++ scripts/install.sh
@@ -19,6 +19,31 @@ packages="zsh git tmux ripgrep stow curl make cmake gcc"
 echo "Installing required packages: $packages"
 sudo apt install -y $pkgs
 
+# SSH
+ssh_filename="id_ed25519"
+if ! [ -f ~/.ssh/$ssh_filename ]; then
+  echo "Cannot detect SSH key, generating a new one."
+
+  ssh-keygen -t ed25519 -f ~/.ssh/$ssh_filename -N '' -C "andrewthomashowell@gmail.com" <<< y >/dev/null 2>&1
+
+  # Ensure ssh-agent is running
+  eval "$(ssh-agent -s) >/dev/null"
+
+  ssh-add ~/.ssh/$ssh_filename
+
+  echo "SSH key generated. The public key is '$(cat ~/.ssh/${ssh_filename}.pub)'"
+fi
+
+
+# Git
+sudo apt upgrade git
+
+git config --global gpg.format ssh
+git config --global user.signingkey ~/.ssh/${ssh_filename}.pub
+git config --global commit.gpgsign true
+git config --global gpg.ssh.allowedSignersFile "~/.config/git/allowed_signers"
+git config --global user.email "andrewthomashowell@gmail.com"
+
 echo "Installing fzf (apt version is outdated)"
 clone_or_pull https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install --completion --key-bindings --no-update-rc --no-bash
@@ -57,6 +82,10 @@ make CMAKE_BUILD_TYPE=RelWithDebInfo CMAKE_INSTALL_PREFIX=/opt/neovim
 sudo make --directory ~/scratch/neovim install
 # Symlink the binary over to local
 sudo ln -sfv /opt/neovim/bin/nvim /usr/local/bin/nvim
+
+echo "Moving .zshrc -> .zshrc.old"
+# Required to ensure stow succeeds.
+mv -iv ~/.zshrc ~/.zshrc.old
 
 echo "Restowing files"
 ~/dotfiles/scripts/restow.sh
